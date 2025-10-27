426cd69a49e356b19463e532772c439c8389a99f
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-10-27 07:27:56 +0530

    CodeDex python course exercise 12 (in 3rd chapter) completed.

---
+++ 12_grades.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+
+grade = float(input('Enter your marks: '))
+
+if grade >= 0 and grade <= 100:
+  if grade >= 55:
+    print('You passed.')
+  else:
+    print('You failed.')
+else:
+  print('Enter valid marks!')
