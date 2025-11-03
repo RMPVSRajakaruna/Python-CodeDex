80bdad28f640f7f8de12e0bd9784238f71289c9f
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-11-03 19:47:16 +0530

    Learn about Lists. CodeDex python course exercise 22 (in 5th chapter) completed.

---
+++ 22_grocery.py
@@ -0,0 +1,11 @@
+# More facts about lists:
+
+# List items allow duplicate values.
+# Lists can have values with different data types.
+# There's no limit to how much data a list can hold.
+
+# Write code below 💖
+
+grocery = ['🥚 Eggs','🥑 Avocados','🍪 Cookies','🌶 Hot Pepper Jam','🫐 Blueberries','🥦 Broccoli']
+
+print(grocery)
