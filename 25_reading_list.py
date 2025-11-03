da8cffc4b19636522e416ad7bf91841a649c6b55
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-11-03 22:46:43 +0530

    Learn about 11 list methods. CodeDex python course exercise 25 (in 5th chapter) completed.

---
+++ 25_reading_list.py
@@ -0,0 +1,23 @@
+# .append()	Add an item to the end of the list
+# .clear()	Remove all items from the list
+# .copy()	Return a shallow copy of the list
+# .count()	Return the number of times the value appears in the list
+# .extend()	Appends another list to the current list by extending it
+# .index()	Returns the index of a value inside the list
+# .insert()	Insert an item at a specified position in the list
+# .pop()	Remove an item from a specified position in the list
+# .remove()	Remove an item from the list based on the value of the item
+# .reverse()	Reverses the list in place
+# .sort()	Sorts the list in place
+
+# Write code below 💖
+
+books = ['Harry Potter',
+'1984',
+'The Fault in Our Stars',
+'The Mom Test',
+'Life in Code']
+
+books.append('Pachinko')
+books.remove('The Fault in Our Stars')
+books.pop('1')
