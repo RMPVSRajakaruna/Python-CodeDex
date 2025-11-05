92d17e33f3bf51b8023490f7c10444371d81ac57
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-11-06 00:36:36 +0530

    Add complete implementation of basic Python functions with examples

---
+++ 28_d.r.y.py
@@ -0,0 +1,20 @@
+# Write code below 💖
+
+# 1) print() -> prints output to console
+print("Hello, this is print()")
+
+# 2) len() -> returns the length of something (string/list/etc)
+my_list = [10, 20, 30]
+print("Length of list:", len(my_list))
+
+# 3) type() -> tells what data type something is
+value = 99.5
+print("Type of value:", type(value))
+
+# 4) sum() -> adds all numbers inside a list
+print("Sum of list:", sum(my_list))
+
+# 5) sorted() -> new function -> returns new sorted version of a list (does not change original)
+# this is built-in too and we haven't used it before
+unsorted_list = [5, 2, 9, 1]
+print("Sorted list:", sorted(unsorted_list))
