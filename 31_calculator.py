5a8c9429667eaa054af3209d004856fdec8593e6
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-11-07 16:36:41 +0530

    Add basic arithmetic operations: addition, subtraction, multiplication, division, and exponentiation. (User-defined functions)

---
+++ 31_calculator.py
@@ -0,0 +1,22 @@
+# Write code below 💖
+
+def add(a, b):
+  answer = a + b
+  return answer
+
+def subtract(a, b):
+  answer = a - b
+  return answer
+
+def multiply(a, b):
+  answer = a * b
+  return answer
+
+def divide(a, b):
+  answer = a/b
+  return answer
+
+def exp(a, b):
+  answer = a ** b
+  return answer
+
