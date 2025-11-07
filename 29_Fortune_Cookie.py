a4ed6e2336fe80671673aab86df167831accb6f7
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-11-07 15:29:18 +0530

    Add fortune cookie fortune generator implementation

---
+++ 29_Fortune_Cookie.py
@@ -0,0 +1,24 @@
+# Write code below 💖
+
+import random
+
+def fortune():
+
+    num = random.randint(1, 8)
+
+    if num == 1:
+        print("Don't pursue happiness – create it.")
+    elif num == 2:
+        print('All things are difficult before they are easy.')
+    elif num == 3:
+        print('The early bird gets the worm, but the second mouse gets the cheese.')
+    elif num == 4:
+        print('Someone in your life needs a letter from you.')
+    elif num == 5:
+        print("Don't just think. Act!")
+    elif num == 6:
+        print('Your heart will skip a beat.')
+    elif num == 7:
+        print('The fortune you search for is in another cookie.')
+    elif num == 8:
+        print("Help! I'm being held prisoner in a Chinese bakery!")
