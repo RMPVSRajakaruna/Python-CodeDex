66122f3bb7b39c56b6517816c0c0704bbe5e5789
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-11-07 23:50:21 +0530

    Add greetings function to display formatted name. (1st challenge of 6th chapter.)

---
+++ James_Bond.py
@@ -0,0 +1,7 @@
+# Write code below 💖
+
+def greetings(first_name, last_name):
+    print(f"{last_name}, {first_name} {last_name}")
+
+# call the function with your name
+greetings("Richard", "Hendricks")
