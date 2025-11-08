d064fba97d91d387dd8c762c25f612d1d4809a79
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-11-08 22:10:21 +0530

    Add Restaurant class with initial attributes for name, category, rating, and delivery. (Using class)

---
+++ 34_restaurants.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+
+# restaurants.py
+
+class Restaurant:
+    def __init__(self):
+        self.name: str = ""
+        self.category: str = ""
+        self.rating: float = 0.0
+        self.delivery: bool = False
