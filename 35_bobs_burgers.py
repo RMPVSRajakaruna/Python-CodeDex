ebabf72c347d61c86255da422d8bfc61fb58cb3c
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-11-09 23:51:53 +0530

    Add initial implementation of restaurant instances and print their attributes. (35 chapter 07)

---
+++ 35_bobs_burgers.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+
+from restaurants import Restaurant
+
+bobs_burgers = Restaurant("Bob's Burgers", "American Diner", 4.7, False)
+
+route_66 = Restaurant("Route 66 Diner", "Western Grill", 4.4, True)
+kfc_kiribathgoda = Restaurant("KFC Kiribathgoda", "Fast Food", 4.2, True)
+
+print(vars(bobs_burgers))
+print(vars(route_66))
+print(vars(kfc_kiribathgoda))
