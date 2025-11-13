50b1947c96c6cc64dd957c6b3f12580881b3a744
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-11-13 10:55:17 +0530

    Add solar system simulation to calculate surface area of random planets

---
+++ 40_solar_system.py
@@ -0,0 +1,34 @@
+
+from math import pi
+from random import choice as ch
+
+planets = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Mars',
+    'Saturn'
+]
+
+random_planet = ch(planets)
+
+if random_planet == 'Mercury':
+    r = 2440
+elif random_planet == 'Venus':
+    r = 6052
+elif random_planet == 'Earth':
+    r = 6371
+elif random_planet == 'Mars':
+    r = 3390
+elif random_planet == 'Saturn':
+    r = 58232
+else:
+    print("Oops! An error occurred.")
+    r = 0  
+
+if r > 0:
+    area = 4 * pi * (r ** 2)
+
+    area = round(area, 2)
+
+    print(f"The surface area of {random_planet} is approximately {area} square kilometers.")
