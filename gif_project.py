2444aa5c167e015e0c2a6caf77a31de37c5af989
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2025-11-15 09:45:25 +0530

    Refactor code structure and remove redundant sections for improved readability and maintainability

---
+++ gif_project.py
@@ -0,0 +1,13 @@
+
+import imageio.v3 as iio
+
+filenames = ['1.png', '2.png']
+
+images = []
+
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('team.gif', images, duration=500, loop=0)
+
+print("✅ GIF created successfully! Saved as 'team.gif'")
